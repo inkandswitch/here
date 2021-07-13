5ad1735383543a4f6cf56c80522d52f57b14617c
Karissa McKelvey <633012+okdistribute@users.noreply.github.com>
2021-07-13 12:00:41 -0700

    add backchannel backend

---
+++ src/backend/index.ts
@@ -0,0 +1,25 @@
+import { Backchannel, BackchannelSettings } from './backchannel';
+import defaultConfig from './config';
+export { ERROR, EVENTS } from './backchannel';
+
+let instance = null;
+
+export default function initialize(
+  _dbName?: string,
+  config?: BackchannelSettings
+): Backchannel {
+  if (instance) return instance;
+  let dbName = _dbName || 'backchannel_' + window.location.hash;
+  instance = new Backchannel(dbName, config || defaultConfig);
+  instance.on('error', function onError(err: Error) {
+    console.error('Connection error');
+    console.error(err);
+  });
+
+  instance.on('close', () => {
+    console.error('close');
+    instance = null;
+  });
+
+  return instance;
+}
