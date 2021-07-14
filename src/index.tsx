c62269a47eab60e0260395b94f1992e8fca57c97
chris <daiyitastic@gmail.com>
2021-07-14 11:17:39 -0700

    add mapbox map

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 import App from './ui/App';
 import reportWebVitals from './reportWebVitals';
+
 declare global {
   interface Window {
     spake2: any;
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,40 +1,43 @@
-import React, { useEffect } from 'react';
-import { ChakraProvider, Button, Stack, Box } from '@chakra-ui/react';
-import styled from '@emotion/styled';
+/** @jsxImportSource @emotion/react */
+import React, { useEffect, useState } from 'react';
+import { ChakraProvider, Button, Stack } from '@chakra-ui/react';
 import PeopleDrawer from './components/PeopleDrawer';
-
-const MapWrapper = styled(Box)`
-  width: 100%;
-  height: 100%;
-`;
+import Map from './components/Map';
 
 function App() {
-  var target, options;
+  const [askedLocation, setAskedLocation] = useState(false);
 
   function success(pos) {
     var crd = pos.coords;
 
     let message = JSON.stringify(crd);
+    console.log(message);
+
+    // TODO send message to backend
   }
 
   function error(err) {
     console.warn('ERROR(' + err.code + '): ' + err.message);
   }
 
-  options = {
-    enableHighAccuracy: false,
-    timeout: 5000,
-    maximumAge: 0,
-  };
-
   useEffect(() => {
-    let id = navigator.geolocation.watchPosition(success, error, options);
-  });
+    if (!askedLocation) {
+      const options = {
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 0,
+      };
 
-  // to clear:  navigator.geolocation.clearWatch(id);
+      const id = navigator.geolocation.watchPosition(success, error, options);
+      setAskedLocation(true);
+
+      return () => navigator.geolocation.clearWatch(id);
+    }
+  }, [askedLocation, setAskedLocation]);
+
   return (
     <ChakraProvider>
-      <MapWrapper>map</MapWrapper>
+      <Map />
       <Stack spacing={4} direction="row" align="center">
         <Button>Share Location</Button>
         <Button>Redeem code</Button>
