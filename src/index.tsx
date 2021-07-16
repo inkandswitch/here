d46d48de33cc23a6477362aa661166d0db689adc
okdistribute <633012+okdistribute@users.noreply.github.com>
2021-07-16 11:36:14 -0400

    fix: listen for open event before rendering app

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 import App from './ui/App';
 import reportWebVitals from './reportWebVitals';
+import Backchannel, { EVENTS } from './backend';
 
 declare global {
   interface Window {
@@ -11,12 +12,16 @@ declare global {
   }
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let backchannel = Backchannel()
+
+backchannel.once(EVENTS.OPEN, async () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
